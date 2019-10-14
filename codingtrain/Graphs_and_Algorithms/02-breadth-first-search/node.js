ace05d054ba63c4b7de7b42e3179b800c2ce45aa
André Vital <santiago_vital@outlook.com>
2019-10-14 17:55:18 -0500

    Finished version without CSS

--- codingtrain/Graphs_and_Algorithms/02-breadth-first-search/node.js
+++ codingtrain/Graphs_and_Algorithms/02-breadth-first-search/node.js
@@ -6,15 +6,14 @@
 // Based on Daniel Shiffman's Coding Challenge Video
 // http://codingtra.in
 
-function Node(value) {
-  this.value = value;
-  this.edges = [];
-  this.searched = false;
-  this.parent = null;
+function Node(value){
+	this.value = value;
+	this.edges = [];
+	this.searched = false;
+	this.parent = null;
 }
 
-Node.prototype.addEdge = function(neighbor) {
-  this.edges.push(neighbor);
-  // Both directions!
-  neighbor.edges.push(this);
+Node.prototype.addEdge = function(node) {
+	this.edges.push(node);
+	node.edges.push(this);
 }
--- codingtrain/Graphs_and_Algorithms/02-breadth-first-search/sketch.js
+++ codingtrain/Graphs_and_Algorithms/02-breadth-first-search/sketch.js
@@ -7,84 +7,84 @@
 // http://codingtra.in
 
 var data;
-var graph;
 var dropdown;
+var graph;
 
 function preload() {
-  data = loadJSON('https://raw.githubusercontent.com/jmmedel/Javascirpt-Code-Chanllenges/0cdaa86651885582353616d04c5aa644f6548e8d/CodingChallenges/CC_068_BFS_kevin_bacon/kevinbacon.json');
+	data = loadJSON('https://raw.githubusercontent.com/jmmedel/Javascirpt-Code-Chanllenges/master/CodingChallenges/CC_068_BFS_kevin_bacon/kevinbacon.json');
 }
 
-function setup() {
-  dropdown = createSelect();
-  dropdown.changed(bfs);
-  noCanvas();
+function setup(){
+	graph = new Graph();
+	dropdown = createSelect();
+	dropdown.changed(bfs);
+	noCanvas();
+	var movies = data.movies;
 
-  var movies = data.movies;
+	for(let i = 0; i < movies.length; i++){
+		var movie = movies[i].title;
+		var movieNode = new Node(movie);
+		graph.addNode(movieNode);
 
-  for (var i = 0; i < movies.length; i++) {
-    var movie = movies[i].title;
-    var cast = movies[i].cast;
-    var movieNode = new Node(movie);
-    graph.addNode(movieNode);
+		var actors = movies[i].cast;
 
-    for (var j = 0; j < cast.length; j++) {
-      var actor = cast[j];
-      var actorNode = graph.getNode(actor);
-      if (actorNode == undefined) {
-        actorNode = new Node(actor);
-        dropdown.option(actor);
-      }
-      graph.addNode(actorNode);
-      movieNode.addEdge(actorNode);
-    }
-  }
+		for(let j = 0; j < actors.length; j++){
+			var actor = actors[j];
+			var actorNode = graph.getNode(actor);
+
+			if(actorNode == undefined) {
+				actorNode = new Node(actor);
+        		dropdown.option(actor);
+        	}
+
+			graph.addNode(actorNode);
+			actorNode.addEdge(movieNode);
+		}
+	}
 }
 
-function bfs() {
-  graph.reset();
-  var start = graph.setStart(dropdown.value());
-  var end = graph.setEnd("Kevin Bacon");
+function bfs(){
+	graph.reset();
+	var start = graph.setStart(dropdown.value());
+	var end = graph.setEnd("Kevin Bacon");
 
-  var queue = [];
+	var queue = [];
+  	var txt = '';
 
-  start.searched = true;
-  queue.push(start);
+	start.searched = true;
+	queue.push(start);
 
-  while (queue.length > 0) {
-    var current = queue.shift();
-    if (current == end) {
-      console.log("Found " + current.value);
-      break;
-    }
-    var edges = current.edges;
-    for (var i = 0; i < edges.length; i++) {
-      var neighbor = edges[i];
-      if (!neighbor.searched) {
-        neighbor.searched = true;
-        neighbor.parent = current;
-        queue.push(neighbor);
-      }
-    }
-  }
+	while(queue.length > 0){
+		var currNode = queue.shift();
+		if(currNode == end)  break;
 
-  var path = [];
-  path.push(end);
-  var next = end.parent;
-  while (next != null) {
-    path.push(next);
-    next = next.parent;
-  }
+		for(let i = 0; i < currNode.edges.length; i++){
+			let next = currNode.edges[i];
+			if(!next.searched){
+				next.searched = true;
+				queue.push(next);
+				next.parent = currNode;
+			}
+		}
+	}
 
-  var txt = '';
-  for (var i = path.length - 1; i >= 0; i--) {
-    var n = path[i];
-    txt += n.value
-    if (i != 0) {
-      // txt += '|';
-      // txt += "\n";
-      // txt += '|';
-      txt += '<br>|<br>|<br>v<br>';
-    }
-  }
-  createP(txt);
+	var path = [];
+
+	path.push(end);
+	let next = end.parent;
+
+	while(next != null){
+		path.push(next);
+		next = next.parent;
+	}
+
+	txt += start.value + " has a Kevin Bacon number of " + (path.length - 2) + "<br><br>";
+
+	for(let i = path.length - 1; i >= 0; i--){
+		var n = path[i].value;
+
+		txt += n;
+		if(i > 0) txt += "<br>|<br>|<br>v<br>";
+	}
+	createP(txt);
 }
